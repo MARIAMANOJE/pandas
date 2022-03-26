6f75c7c926fc6843d610bf556da16bbd05b703f0
MARIAMANOJE <96276515+MARIAMANOJE@users.noreply.github.com>
2022-03-26 12:27:57 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 28 21:06:28 2021
+Created on Sat Mar 26 12:27:30 2022
 
-@author: Manoj
+@author: Mano
 """
+
+
 import pandas as pd
-file = "C:/Users/Manoj/Documents/pandas/a and b.xlsx"
+file = "C:/Users/Mano/Documents/pandas/a and b.xlsx"#choosefilepathwhereyousave
 newData = pd.read_excel(file)
-print("mean=", newData.mean())
-print("median=", newData.median())
-print('mode=', newData.mode())
-print("std="newData.std())
-print("var"newData.var())'
-print("KURT"newdata.kurt())
+print("mean", newData.mean())
+print("median", newData.median())
+print('mode', newData.mode())
+print("std",newData.std())
+print("var",newData.var())
+print("KURT",newData.kurt())
