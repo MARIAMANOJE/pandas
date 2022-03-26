ab36019487c87f1a7f8535206251e80526e7bf9d
MARIAMANOJE <96276515+MARIAMANOJE@users.noreply.github.com>
2022-03-26 12:17:55 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -5,13 +5,11 @@ Created on Tue Dec 28 21:06:28 2021
 @author: Manoj
 """
 import pandas as pd
-import pandas as ph
 file = "C:/Users/Manoj/Documents/pandas/a and b.xlsx"
-newData = ph.read_excel(file)
-newData
-print("mean", newData.mean())
-print("median", newData.median())
-print('mode', newData.mode())
-print(newData.std())
-print(newData.var())
-
+newData = pd.read_excel(file)
+print("mean=", newData.mean())
+print("median=", newData.median())
+print('mode=', newData.mode())
+print("std="newData.std())
+print("var"newData.var())'
+print("KURT"newdata.kurt())
